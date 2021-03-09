ff23feb262384f5e43ba897c8dc3b9fdfa5a6d4e
Yuliana Sadovskaya <juliana.sad07@gmail.com>
2021-03-09 23:02:59 +0300

    init: complet "typical-arrays-problems" task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array === 'undefined' || !array.length){
+    return 0;
+  } else {
+    return array.sort((a, b) => a - b)[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array === 'undefined' || !array.length){
+    return 0;
+  } else {
+    return array.sort((a, b) => b - a)[0];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array === 'undefined' || !array.length){
+    return 0;
+  } else {
+    return array.reduce((acc, cur, i) => 
+      {
+        if (i === array.length - 1){
+          return (acc + cur)/(i+1);
+        } else {
+          return acc + cur;
+        }
+      }, 0);
+  }
 }
